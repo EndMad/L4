481ff5761d492ac53c4b17e1b9fd7fc27949eea5
EndMad <70961155+EndMad@users.noreply.github.com>
2021-01-27 09:39:52 +0300

    Update individual_1.py

--- individual_1.py
+++ individual_1.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Дано предложение. Вывести «столбиком» все его буквы и,
+# стоящие на четных местах.
+
 print("Введите предложение")
-print(*(letter for i, letter in enumerate(input()) if letter == "и" and not i % 2), sep="\n")
+print(
+    *(letter for i,
+        letter in enumerate(input()) if letter == "и" and not i % 2),
+    sep="\n"
+)
