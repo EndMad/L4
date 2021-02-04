89527409d6446be6fd91846c1a83d178825804f8
EndMad <70961155+EndMad@users.noreply.github.com>
2021-02-04 22:25:50 +0300

    Update individual_increased_complexity.py

--- individual_increased_complexity.py
+++ individual_increased_complexity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Даны три слова. 
+# Даны три слова.
 # Напечатать неповторяющиеся в них буквы.
 
-word = input("Введите три слова ")
-
-while word:
-    if word.count(word[0]) == 1:
-        print(word[0], ':', word.count(word[0]))
-    word = word.replace(word[0], '')
+if __name__ == '__main__':
+    word = input("Введите три слова ")
+    while word:
+        if word.count(word[0]) == 1:
+            print(word[0], ':', word.count(word[0]))
+        word = word.replace(word[0], '')
